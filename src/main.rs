dbf223bb0a2e0dd6dd74b0bc6fe45ef583cbaf9f
Nertsal <sasha.kudasov04@gmail.com>
2023-07-14 20:46:08 +0300

    Setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use geng::prelude::*;
+
+#[derive(clap::Parser)]
+struct Opts {
+    #[clap(flatten)]
+    window: geng::CliArgs,
+}
+
+#[derive(geng::asset::Load)]
+pub struct Assets {
+    pub config: Config,
+}
+
+#[derive(geng::asset::Load, Deserialize)]
+#[load(serde = "ron")]
+pub struct Config {
+    background_color: Rgba<f32>,
+    object_colors: Vec<Rgba<f32>>,
+}
+
+pub struct State {
+    geng: Geng,
+    assets: Rc<Assets>,
+    paused: bool,
+}
+
+impl State {
+    pub fn new(geng: Geng, assets: Rc<Assets>) -> Self {
+        Self {
+            paused: false,
+            geng,
+            assets,
+        }
+    }
+}
+
+impl geng::State for State {
+    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        ugli::clear(
+            framebuffer,
+            Some(self.assets.config.background_color),
+            Some(1.0),
+            None,
+        );
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    logger::init();
+
+    let opts: Opts = clap::Parser::parse();
+
+    let mut context = geng::ContextOptions::default();
+    context.with_cli(&opts.window);
+    Geng::run_with(&context, |geng| async move {
+        let manager = geng.asset_manager();
+        let assets: Rc<Assets> = geng::asset::Load::load(manager, &run_dir().join("assets"), &())
+            .await
+            .expect("failed to load assets");
+        geng.run_state(State::new(geng.clone(), assets)).await
+    })
 }
